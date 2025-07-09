9e296b806760dd6265cd2fbf6f973f157aaaea0e
aryankandpal04 <aryankandpal04@gmail.com>
2025-07-09 15:36:42 +0530

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// Introduction to js
+
+console.log("hello js dev");
+
+// variables in js
+
+fullName = "Aryan Kandpal";
+rollNo = 16;
+marks = 7.86;
+pass = true;
+back = null;
+bloodGroup = undefined;
+
+console.log(fullName, rollNo, marks, pass, back, bloodGroup);
