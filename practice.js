b4ceb3979c79af338f0cce32fe4c67430be982a2
aryankandpal04 <aryankandpal04@gmail.com>
2025-07-14 17:36:03 +0530

    update

--- practice.js
+++ practice.js
@@ -3,13 +3,14 @@
 {
     //Q1 Get user to input a number using prompt("Enter a number:"). Check if the number is a multiple of 5 or not.
     let num = prompt("enter a number : ");
+    num = Number(num);
     if (num % 5 === 0){
-        alert("this number is multiple by 5");
-        console.log("this is multiple by 5");
+        alert(`this number ${num} is multiple by 5`);
+        console.log(`this number ${num} is multiple by 5`);
     }
     else{
-        alert("this number is not multiple by 5");
-        console.log("this is multiple by 5");
+        alert(`this number ${num} is not multiple by 5`);
+        console.log(`this number ${num} is multiple by 5`);
     }
     
 }
