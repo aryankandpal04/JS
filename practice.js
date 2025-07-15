61206714ac0730ec02bc6bb3d7c3064f1082554a
aryankandpal04 <aryankandpal04@gmail.com>
2025-07-15 13:58:48 +0530

    update

--- practice.js
+++ practice.js
@@ -19,7 +19,7 @@
 {
     /*
     Q2 Write a code which can give grades to students according to their scores.
-      -  80-100, A
+      -  90-100, A
       -  70-89, B
       -  60-69, C
       -  50-59, D
@@ -29,26 +29,21 @@
     let marks = prompt("enter your marks : ");
     marks = Number(marks);
 
-    if(marks === 100 && marks <=80){
+    if (marks >= 90 && marks <= 100){
         console.log("A");
     }
-
-    else if(marks >= 70 && marks <= 89){
+    else if (marks >= 70 && marks <=89){
         console.log("B");
     }
-
-    else if(marks >= 60 && marks <= 69){
+    else if (marks >= 60 && marks <=69 ){
         console.log("C")
     }
-
-    else if(marks >= 50 && marks <= 59){
-        console.log("D");
+    else if (marks >= 50 && marks >= 50 ){
+        console.log("D")
     }
-
-    else if (marks === 0 && marks <= 49){
-        console.log("F");
+    else if (marks >= 0 && marks >= 49 ){
+        console.log("F")
     }
-
     else{
         console.log("Invalid Input!");
     }
